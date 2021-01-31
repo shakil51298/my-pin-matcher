15a6020ef2aa19ddceb658fcf1150061ad62b6b0
shakil51298 <zikaiwen02@gmail.com>
2021-02-01 02:11:42 +0800

    matcher

---
+++ Draf.js
@@ -0,0 +1,13 @@
+RANDOM()
+var NumberToString = split+'';
+for (let i = 0; i < NumberToSting.length; i++) {
+    const string = NumberToSting[i];
+    
+}
+if()
+
+function RANDOM(){
+    var Random = (Math.random()*10000+'');
+var split = Random.split('.')[0];
+console.log(split);
+}
